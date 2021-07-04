9ce71b1dd5b90ec811df5f3fcf587ea45e10a03a
iVlady <vinogrotskiy97@gmail.com>
2021-07-04 20:37:58 +0300

    own

--- RandomJokeNew/View Controllers/OwnListTableViewController.swift
+++ RandomJokeNew/View Controllers/OwnListTableViewController.swift
@@ -78,6 +78,13 @@ class OwnListTableViewController: UITableViewController, AddJokeDelegate {
 
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            ownJoke.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 
     
 
